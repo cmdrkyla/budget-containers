494122640bb1c122bb1947f6d0d4d7b3393aa37f
Kyla Eadie <eadiek@gmail.com>
2023-09-01 16:37:37 -0400

    Renamed unittest files to include unit

--- api/pytests/unit/auth/test_unit_auth.py
+++ api/pytests/unit/auth/test_unit_auth.py
@@ -50,28 +50,6 @@ class TestAuthLogin:
         # Then - forbidden status code
         assert response.status_code == 401
 
-
-class TestAuthLogoutE2E:
-    @mock.patch.object(Auth, "authenticate")
-    def test__auth_logout(self, mock_auth_authenticate):
-        # Given - we currently have a valid session (login)
-        valid_email_address = "valid_email_address"
-        valid_password = "valid_password"
-        mock_auth_authenticate.return_value = True
-        app.test_client().post(
-            "/api/auth/login",
-            data={
-                "email_address": valid_email_address,
-                "password": valid_password
-            },
-        )
-
-        # When - we logout
-        response = app.test_client().get("api/auth/logout")
-
-        # Then - the session data is destroyed
-        assert response.status_code == 200
-
     
 class TestAuthAuthenticate:
     @mock.patch.object(Password, "verify_password")
